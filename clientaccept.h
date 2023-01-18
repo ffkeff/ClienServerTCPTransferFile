7b9c5f1a7e9dc40d33a64be4073536c8e8eb796c
Ivan <111454954+ffkeff@users.noreply.github.com>
2023-01-18 22:52:44 +0000

    #2

--- clientaccept.h
+++ clientaccept.h
@@ -1,9 +1,9 @@
 #ifndef CLIENTACCEPT_H
 #define CLIENTACCEPT_H
 
-int MakeSocket(char* host, int port);
+int MakeSocket(char *host, char *port);
 
-void WriteDataToFile(char* path_name, char* data, int file_size);
+void WriteDataToFile(char *path_name, char *data, int file_size);
 
 void AcceptFile(int sfd);
 
